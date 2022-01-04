ee096fd9c089191d88aeb7b9662cdbf0d0c9a589
Yogesh1420 <demonyogz@gmail.com>
2022-01-04 14:53:07 +0530

    Committed succesfully

---
+++ testProj/src/com/mph/view/Mainclass.java
@@ -0,0 +1,12 @@
+package com.mph.view;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+	    System.out.println("welcome to our project");
+	    Method m = new Method();
+	    m.Method1();
+	    m.Method2();
+	}
+
+}
---
+++ testProj/src/com/mph/view/Method.java
@@ -0,0 +1,16 @@
+package com.mph.view;
+
+public class Method {
+
+	public void Method1() {
+		System.out.println("Good Morning");
+		
+	}
+	public void Method2() {
+		System.out.println("Good afternoon");
+		
+	}
+	
+	
+
+}
